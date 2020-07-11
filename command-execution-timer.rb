38195dde6a826da41314515e0321942b9f144e01
Henry Bley-Vroman <olets@olets.dev>
2020-07-11 15:20:31 -0400

    feat(command execution time): update sha

--- command-execution-timer.rb
+++ command-execution-timer.rb
@@ -2,7 +2,7 @@ class CommandExecutionTimer < Formula
   desc "A zsh plugin for timing, working with, and displaying the time an interactive shell command takes to execute."
   homepage "https://github.com/olets/command-execution-timer"
   url "https://github.com/olets/command-execution-timer/releases/download/v1.0.0.tar.gz"
-  sha256 "39af92c2747aa68d6e30e2907a1042f3485932aaa39d9b6877d444d263c0a318"
+  sha256 "f36c3896974bc10fe4c6bef6b6f3f93ecbc0195dfbcce78d617469ab2188399f"
   head "https://github.com/olets/command-execution-timer.git", :branch => "main"
 
   bottle :unneeded
