c5facb2a72ca8197aff87aef67f9e78a2cb95378
Henry Bley-Vroman <olets@olets.dev>
2020-07-26 14:00:54 -0400

    chore(zsh-abbr): point to v3.3.4, and 'brew audit' fixes

--- zsh-abbr.rb
+++ zsh-abbr.rb
@@ -1,9 +1,9 @@
 class ZshAbbr < Formula
-  desc "zsh abbreviations, inspired by fish's abbr and enhanced"
+  desc "The zsh manager for auto-expanding abbreviations, inspired by fish"
   homepage "https://github.com/olets/zsh-abbr"
-  url "https://github.com/olets/zsh-abbr/releases/download/v3.3.3/v3.3.3.tar.gz"
-  sha256 "b4e6689f1701113adff28c3e93f8b27d457fba897b972779a8701a5493926b2f"
-  head "https://github.com/olets/zsh-abbr.git"
+  url "https://github.com/olets/zsh-abbr/releases/download/v3.3.4/v3.3.4.tar.gz"
+  sha256 "8324f32c975489146d60b92fb4668ee3d067186aacea17767efc85fb68a43a72"
+  head "https://github.com/olets/zsh-abbr.git", :branch => "master"
 
   bottle :unneeded
 
@@ -22,18 +22,18 @@ class ZshAbbr < Formula
         source #{HOMEBREW_PREFIX}/share/zsh-abbr/zsh-abbr.zsh
 
       If you prefer to manage the package with Homebrew but load it with
-      a plugin manager, rather than sourcing the above path
-      use your plugin manager to load it.
+      a plugin manager, use your plugin manager to load the above path
+      rather than `source`ing it.
 
-      Then to activate abbreviations in already open sessions, reload .zshrc:
+      Then to activate abbreviations in already open sessions, restart zsh:
 
-        source ~/.zshrc
+        exec zsh
 
     EOS
   end
 
   test do
-    assert_match '1',
+    assert_match "1",
       shell_output("zsh -c '. #{pkgshare}/zsh-abbr.zsh && echo $ZSH_ABBR_DEFAULT_BINDINGS'")
   end
 end
