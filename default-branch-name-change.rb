7223f832516ed4d7fdcc0be62b9eac5ccf9d990f
Henry Bley-Vroman <olets@olets.dev>
2023-02-10 16:13:16 -1000

    feat(homebrew-default-branch-change): publish v1.0.0.beta-1

---
+++ default-branch-name-change.rb
@@ -0,0 +1,7 @@
+class HomebrewDefaultBranchChange < Formula
+  desc "Automate the rebasing of Git branches and creation of stage branches"
+  homepage "https://github.com/olets/homebrew-default-branch-change"
+  url "https://github.com/olets/homebrew-default-branch-change/archive/refs/tags/v1.0.0.beta-1.tar.gz"
+  sha256 "2bbf152056d36ad62239e50e8c49307ca5de39dd6d0b86f644aa371b059ded05"
+  head "https://github.com/olets/homebrew-default-branch-change", branch: "original-default-branch-name"
+end
