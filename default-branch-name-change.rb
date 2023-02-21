bb69b3a22d4df928bc1987930f45ca5ad20cdc80
Henry Bley-Vroman <bley.vroman@gmail.com>
2023-02-20 16:51:31 -1000

    feat(default-branch-name-change): publish 1.0.0.beta-8
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- default-branch-name-change.rb
+++ default-branch-name-change.rb
@@ -1,7 +1,7 @@
 class DefaultBranchNameChange < Formula
   desc "Automate the rebasing of Git branches and creation of stage branches"
   homepage "https://github.com/olets/homebrew-default-branch-change"
-  url "https://github.com/olets/homebrew-default-branch-change/archive/refs/tags/v1.0.0.beta-1.tar.gz"
-  sha256 "2bbf152056d36ad62239e50e8c49307ca5de39dd6d0b86f644aa371b059ded05"
+  url "https://github.com/olets/homebrew-default-branch-change/archive/v1.0.0.beta-8.tar.gz"
+  sha256 "c88fa5b5154f80163e932fd0ae47b7cd7b74a7480df2e242870985ba98873c63"
   head "https://github.com/olets/homebrew-default-branch-change", branch: "original-default-branch-name"
 end
