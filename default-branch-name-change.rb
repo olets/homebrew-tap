c95da9760a1697dc36a1ca2946c655395300c7c8
Henry Bley-Vroman <olets@olets.dev>
2023-12-06 08:21:25 -1000

    feat(homebrew tests): rename from default-branch-name-change

--- default-branch-name-change.rb
+++
@@ -1,7 +0,0 @@
-class DefaultBranchNameChange < Formula
-  desc "Seeing how well Homebrew handles repo default branch changes"
-  homepage "https://github.com/olets/homebrew-default-branch-change"
-  url "https://github.com/olets/homebrew-default-branch-change/archive/refs/heads/v1.0.0.beta-8.tar.gz"
-  sha256 "c88fa5b5154f80163e932fd0ae47b7cd7b74a7480df2e242870985ba98873c63"
-  head "https://github.com/olets/homebrew-default-branch-change", branch: "original-default-branch-name"
-end
---
+++ homebrew-tests.rb
@@ -0,0 +1,7 @@
+class HomebrewTests < Formula
+  desc "Testing Homebrew questions"
+  homepage "https://github.com/olets/homebrew-tests"
+  url "https://github.com/olets/homebrew-default-branch-change/archive/refs/heads/v1.0.0.beta-8.tar.gz"
+  sha256 "c88fa5b5154f80163e932fd0ae47b7cd7b74a7480df2e242870985ba98873c63"
+  head "https://github.com/olets/homebrew-tests.git", branch: "original-default-branch-name"
+end
