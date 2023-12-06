287ab22064f444a0fc331284d39875e81635b378
Henry Bley-Vroman <olets@olets.dev>
2023-12-06 08:23:40 -1000

    fix(homebrew tests): typo

--- homebrew-tests.rb
+++ homebrew-tests.rb
@@ -1,7 +1,7 @@
 class HomebrewTests < Formula
   desc "Testing Homebrew questions"
   homepage "https://github.com/olets/homebrew-tests"
-  url "https://github.com/olets/homebrew-tests.git"
+  url "https://github.com/olets/homebrew-tests.git",
       tag:      "v1.0.0-beta.9",
       revision: "b06f2e860bb7387565fe7e87697f53e39635c2f3"
   head "https://github.com/olets/homebrew-tests.git", branch: "updated-default-branch-name"
