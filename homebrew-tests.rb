b5b6f94e290309bbdb20de2552eee2b0e3e8ea37
Henry Bley-Vroman <bley.vroman@gmail.com>
2023-12-06 10:11:13 -1000

    feat(homebrew-tests): publish 2.1.3
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- homebrew-tests.rb
+++ homebrew-tests.rb
@@ -2,7 +2,7 @@ class HomebrewTests < Formula
   desc "Testing Homebrew questions"
   homepage "https://github.com/olets/homebrew-tests"
   url "https://github.com/olets/homebrew-tests.git",
-      tag:      "v2.1.2",
-      revision: "8dc27f3cfcdebcdad0bc0719e8542a05b6e76d9a"
+      tag:      "v2.1.3",
+      revision: "c5015866c77c2bf0cedf9b70cb64eca93594c458"
   head "https://github.com/olets/homebrew-tests.git", branch: "updated-default-branch-name"
 end
