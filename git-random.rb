35386650a98f8624568c532c51cc78882d3b45c7
Henry Bley-Vroman <bley.vroman@gmail.com>
2024-08-12 14:42:09 -1000

    feat(git-random): publish 2.0.0
    Created by https://github.com/mislav/bump-homebrew-formula-action
    via the https://github.com/olets/git-random "bump-homebrew-formula-on-release-publication" workflow

--- git-random.rb
+++ git-random.rb
@@ -1,8 +1,8 @@
 class GitRandom < Formula
   desc "Create random-content Git commits, for experimentation and learning"
   homepage "https://github.com/olets/git-random"
-  url "https://github.com/olets/git-random/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "0b06d4c1dfb6fed05baae0696190e2520e19ce5ea8b2b750cbb01fff9b355808"
+  url "https://github.com/olets/git-random/archive/refs/tags/v2.0.0.tar.gz"
+  sha256 "2f73e1102a7d071c8b22fe5f78511f8c156b8c27ec3a7e5945659097d915c8e8"
   head "https://github.com/olets/git-random.git", branch: "main"
 
   def install
