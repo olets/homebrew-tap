89979394b85a03569e0e21007c0e56f0d731330b
Henry Bley-Vroman <olets@olets.dev>
2024-10-27 14:32:32 -1000

    feat(zsh-job-queue): add main and v1 formulae

---
+++ zsh-job-queue.rb
@@ -0,0 +1,39 @@
+class ZshJobQueue < Formula
+  desc "Zsh global synchronous job queue plugin"
+  homepage "https://github.com/olets/zsh-job-queue"
+  url "https://github.com/olets/zsh-job-queue/releases/download/v1.0.0.tar.gz"
+  sha256 "f0cdfeee0b2258857d3e3bcf5be6a1210810094e8d441abc71bfeb0005aa5bc0"
+  head "https://github.com/olets/zsh-job-queue.git", branch: "main"
+
+  def install
+    pkgshare.install "zsh-job-queue.zsh"
+  end
+
+  def caveats
+    <<~EOS
+
+      To activate zsh-job-queue, add the following at the end of your .zshrc:
+
+        source #{HOMEBREW_PREFIX}/share/zsh-job-queue/zsh-job-queue.zsh
+
+      If you prefer to manage the package with Homebrew but load it with
+      a plugin manager, you may be able to load the above path with your
+      plugin manager.
+
+      Then restart zsh. Run:
+
+        exec zsh
+
+      You have installed the latest version of Command Execution Timer.
+
+      Refer to https://github.com/olets/zsh-job-queue/ for more information,
+      including how to pin to version 1.x.
+
+    EOS
+  end
+
+  test do
+    assert_match "0",
+      shell_output("zsh -c '. #{pkgshare}/zsh-job-queue.zsh && [[ -n $JOB_QUEUE_DEBUG ]]; echo $?'")
+  end
+end
---
+++ zsh-job-queue@1.rb
@@ -0,0 +1,39 @@
+class ZshJobQueueAT1 < Formula
+  desc "Zsh global synchronous job queue plugin. Version 1.x"
+  homepage "https://github.com/olets/zsh-job-queue"
+  url "https://github.com/olets/zsh-job-queue/releases/download/v1.0.0.tar.gz"
+  sha256 "f0cdfeee0b2258857d3e3bcf5be6a1210810094e8d441abc71bfeb0005aa5bc0"
+  head "https://github.com/olets/zsh-job-queue.git", branch: "v1"
+
+  def install
+    pkgshare.install "zsh-job-queue.zsh"
+  end
+
+  def caveats
+    <<~EOS
+
+      To activate zsh-job-queue, add the following at the end of your .zshrc:
+
+        source #{HOMEBREW_PREFIX}/share/zsh-job-queue/zsh-job-queue.zsh
+
+      If you prefer to manage the package with Homebrew but load it with
+      a plugin manager, you may be able to load the above path with your
+      plugin manager.
+
+      Then restart zsh. Run:
+
+        exec zsh
+
+      You have installed the latest 1.x version of Command Execution Timer.
+
+      Refer to https://github.com/olets/zsh-job-queue/ for more information,
+      including whether there's a newer major version available.
+
+    EOS
+  end
+
+  test do
+    assert_match "0",
+      shell_output("zsh -c '. #{pkgshare}/zsh-job-queue.zsh && [[ -n $JOB_QUEUE_DEBUG ]]; echo $?'")
+  end
+end
