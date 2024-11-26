47d917eafadb25d3bb573e93b824b493dd9811a1
Henry Bley-Vroman <olets@olets.dev>
2024-11-25 16:46:16 -1000

    feat(git-activity,git-activity@1): add

---
+++ git-activity@1.rb
@@ -0,0 +1,21 @@
+class GitActivityAT1 < Formula
+  desc "Record and show your Git activity across multiple repos, optionally filtered. Version 1.x"
+  homepage "https://github.com/olets/git-activity"
+  url "https://github.com/olets/git-activity/archive/refs/tags/v1.0.0-beta.1.tar.gz"
+  sha256 "90767a260971b45ab220063c18248a539645347ec30f094c3deda4f96f15e054"
+  head "https://github.com/olets/git-activity.git", branch: "v1"
+
+  depends_on "coreutils"
+
+  def install
+    bin.install "git-activity"
+
+    man1.mkpath
+    man1.install "man/man1/git-activity.1"
+  end
+
+  test do
+    assert_match "0",
+      shell_output("zsh -c 'command -v git-activity && echo 0'")
+  end
+end
