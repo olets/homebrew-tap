1897b935a98a975da7006e3598c1daecc177cb73
Henry Bley-Vroman <bley.vroman@gmail.com>
2024-11-27 14:50:37 -1000

    feat(git-activity): publish 1.1.1
    Created by https://github.com/mislav/bump-homebrew-formula-action
    via the https://github.com/olets/git-activity "bump-homebrew-formula-on-release-publication" workflow

--- git-activity.rb
+++ git-activity.rb
@@ -1,8 +1,8 @@
 class GitActivity < Formula
   desc "Record and show your Git activity across multiple repos, optionally filtered"
   homepage "https://github.com/olets/git-activity"
-  url "https://github.com/olets/git-activity/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "92ffbafd7800161d82febedede55cbef3b4a68bb6b3e75d6b7e35f11d7bbaca7"
+  url "https://github.com/olets/git-activity/archive/refs/tags/v1.1.1.tar.gz"
+  sha256 "7218c205874e2903d664f623226a18353fd6e3e36bc10cbdbdd212acfe58bb79"
   head "https://github.com/olets/git-activity.git", branch: "main"
 
   depends_on "awk"
