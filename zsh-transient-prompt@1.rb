5f1653325d96731d16775afa50be4b262b47b069
Henry Bley-Vroman <olets@olets.dev>
2025-01-28 12:46:37 -1000

    feat(zsh-transient-prompt): move to github

--- zsh-transient-prompt@1.rb
+++ zsh-transient-prompt@1.rb
@@ -1,9 +1,9 @@
 class ZshTransientPromptAT1 < Formula
   desc "Add a transient prompt to your zsh command line. Version 1.x"
-  homepage "https://codeberg.org/olets/zsh-transient-prompt"
-  url "https://codeberg.org/olets/zsh-transient-prompt/archive/v1.0.1.tar.gz"
+  homepage "https://github.com/olets/zsh-transient-prompt"
+  url "https://github.com/olets/zsh-transient-prompt/archive/refs/tags/v1.0.1.tar.gz"
   sha256 "5d3a30e79bdec272db307f156a8e498d875d8b2464760d4c93dd283840face84"
-  head "https://codeberg.org/olets/zsh-transient-prompt.git", branch: "v1"
+  head "https://github.com/olets/zsh-transient-prompt.git", branch: "v1"
 
   def install
     pkgshare.install "transient-prompt.zsh-theme"
